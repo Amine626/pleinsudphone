041995d9b0401deed1b7859ee969a79c5e9c18b5
Amine626 <amineelghamri11@gmail.com>
2023-05-01 15:42:35 +0200

    MODIFICATION DU SITE

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-$(".header__navbar-toggle").click(function (e) {
-  e.preventDefault();
-  $(".header__navbar").toggleClass("is-open");
-});
+//Ajoute un écouteur d'événement de clic sur l'élément ".header__navbar-toggle".
+//Lorsque l'élément est cliqué, la classe "is-open" est ajoutée ou supprimée de l'élément ".header__navbar".
+
+document
+  .querySelector(".header__navbar-toggle")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(".header__navbar").classList.toggle("is-open");
+  });
